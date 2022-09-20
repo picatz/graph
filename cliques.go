56f0018682ea0a2bc88a3858910c17f5700ee710
Kent 'picat' Gruber <kent.picat.gruber@gmail.com>
2022-09-20 01:01:28 +0000

    Cleanup cliques

--- cliques.go
+++ cliques.go
@@ -1,6 +1,5 @@
 package graph
 
-
 // Clique is a subset of nodes in a graph such that every two
 // distinct nodes in the set are adjacent.
 //
@@ -28,19 +27,20 @@ func (cliques Cliques) ContainsNode(n *Node) bool {
 	return false
 }
 
-func (cliques Cliques) ContainsNodeWithIndex(n *Node) (int, bool) {
-	for index, clique := range cliques {
-		if clique.Contains(n) {
-			return index, true
-		}
-	}
-	return 0, false
-}
-
 // FindCliques handles finding all "cliques" within a graph. A a clique
 // is a subset of nodes in a graph such that every two distinct nodes
 // in the clique are adjacent. That is, a clique of a graph "G" is an
 // induced subgraph of "G" that is complete.
+//
+// Example
+//
+//	       b
+//	     ↙   ↖
+//	   c       a
+//	 ↙   ↘   ↗
+//	e  →   d
+//
+// Cliques: [1] {c, e, d}
 //
 // References
 // - https://en.wikipedia.org/wiki/Clique_(graph_theory)
@@ -49,15 +49,6 @@ func (cliques Cliques) ContainsNodeWithIndex(n *Node) (int, bool) {
 // - https://mathworld.wolfram.com/Clique.html
 func FindCliques(root *Node, minSize int) Cliques {
 	cliques := Cliques{}
-
-	//           b
-	//         ↙   ↖
-	//       c       a
-	//     ↙   ↘   ↗
-	//    e  →   d
-	//
-	//
-	// Cliques: [1] {c, e, d}
 
 	root.VisitAll(func(n *Node) {
 		if len(n.Edges) == 0 {
@@ -79,19 +70,6 @@ func FindCliques(root *Node, minSize int) Cliques {
 			cliques = append(cliques, clique)
 		}
 	})
-
-	// Basically a tree structure...
-	// groups := map[*Node]NodeSet{}
-	// visitAll(root, nil, func(n *Node) {
-	// 	fmt.Println(n.Name)
-	// 	_, ok := groups[n]
-	// 	if !ok {
-	// 		groups[n] = NodeSet{}
-	// 	}
-	// 	for _, edge := range n.Edges {
-	// 		groups[n][edge.Node] = struct{}{}
-	// 	}
-	// })
 
 	return cliques
 }
